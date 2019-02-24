01a92da41e4c1d72a22d0fc80eca2d363b6cbf1d
Erickson Zhuang <bingluen0224@gmail.com>
2019-02-25 01:17:27 +0800

    Implement repo item component

---
+++ src/components/RepoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="px-2 py-3 col-12 col-md-6 col-lg-3">
+    <b-card
+    style="min-height: 100%"
+    >
+      <div slot="header">
+        <b-img class="owner-avatar" :src="repo.owner.avatar_url" thumbnail="" />
+        <span class="ml-2 font-weight-bold">{{ repo.owner.login }}</span>
+      </div>
+      <b-card-title>
+        <a :href="repo.html_url" target="_blank">{{ repo.full_name }}</a>
+      </b-card-title>
+      <b-card-text>
+        {{ repo.description }}
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RepoItem',
+  props: {
+    repo: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="stylus">
+.owner-avatar
+  max-width: 50px !important
+.card-header
+  padding .375rem !important
+</style>
