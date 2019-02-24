60e47d59b9e1aae4d96aed09946653c99492ff84
Erickson Zhuang <bingluen0224@gmail.com>
2019-02-25 02:48:22 +0800

    Show more information for repo

--- src/components/RepoItem.vue
+++ src/components/RepoItem.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="px-2 py-3 col-12 col-md-6 col-lg-3">
     <b-card
-    style="min-height: 100%"
+    style="min-height: 100%;"
+    class="d-flex"
     >
       <div slot="header">
         <b-img class="owner-avatar" :src="repo.owner.avatar_url" thumbnail="" />
         <span class="ml-2 font-weight-bold">{{ repo.owner.login }}</span>
       </div>
+      <b-card-text v-if="repo.language">
+        <span class="font-weight-bold">{{ repo.language }}</span>
+      </b-card-text>
       <b-card-title>
         <a :href="repo.html_url" target="_blank">{{ repo.full_name }}</a>
       </b-card-title>
       <b-card-text>
         {{ repo.description }}
       </b-card-text>
+      <div class="text-right flex-fill blockquote-footer">
+        {{ repo.license ? repo.license.name : 'unknown license' }}
+      </div>
     </b-card>
   </div>
 </template>
