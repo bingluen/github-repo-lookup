8c3723cf985ef77f0f5288fefc13e36e42381663
Erickson Zhuang <bingluen0224@gmail.com>
2019-02-25 01:18:03 +0800

    Implement SearchBox component

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="search-box">
+    <b-form @submit.prevent="() => true">
+      <b-form-group>
+        <b-form-input
+          v-model="keyword"/>
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+
+export default {
+  name: 'SearchBox',
+  props: {
+    keywordChanged: Function,
+  },
+  data() {
+    return {
+      keyword: '',
+    };
+  },
+  watch: {
+    keyword(newValue) {
+      this.submitKeyword(this, newValue);
+    },
+  },
+  methods: {
+    submitKeyword: _.debounce((that, newValue) => {
+      that.$emit('keywordChanged', newValue);
+    }, 500),
+  },
+};
+</script>
